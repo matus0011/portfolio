86f4174bf2569772a66fbc9c3b99d5054cfb16f9
Mateusz Klich <klichu0011@gmail.com>
2026-05-26 19:06:34 +0200

    refactor: remove unused dependencies, integrations, and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 import glsl from 'vite-plugin-glsl';
-import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    svelte(),
-    icon(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [svelte()],
   i18n: {
     defaultLocale: 'pl',
     locales: ['pl', 'en'],
