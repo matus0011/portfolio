8df94f445b0c5186cf9a9aa37b84f29ff7dabbe5
Mateusz Klich <klichu0011@gmail.com>
2026-06-02 21:36:31 +0200

    refactor: remove gesture control feature and associated UI state

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -6,7 +6,6 @@
   import { t, type Lang } from "../locales";
   import { scrambleTo, DIGIT_CHARS } from "../utils/scramble";
   import { ui } from "../state/ui.svelte";
-  import GestureControl from "./GestureControl.svelte";
 
   let heroSectionEl: HTMLElement;
 
@@ -325,7 +324,6 @@
     >
       <!-- LinkedIn link + location -->
       <div class="absolute bottom-0 left-0 flex flex-col gap-[2px]">
-        <GestureControl {lang} />
         <a
         href="#"
         class="label inline-flex items-center gap-1.5 text-accent transition-colors"
